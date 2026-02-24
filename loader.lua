bc142e419e78c4eaa4d6852e3554628e14efdebb
akktel0987-boop <akktel0987@gmail.com>
2026-02-25 02:31:14 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,15 @@
-local whitelist="https://raw.githubusercontent.com/akktel0987-boop/mew/main/whitelist.txt"
-local main="https://raw.githubusercontent.com/akktel0987-boop/mew/main/main.lua"
+local WHITELIST_URL="https://raw.githubusercontent.com/akktel0987-boop/mew/main/whitelist.txt"
+local MAIN_URL="https://raw.githubusercontent.com/akktel0987-boop/mew/main/main.lua"
+
+getgenv()[string.char(95,75,69,89)]="Q6L6_PRIVATE_KEY_2025"
 
 local p=game:GetService("Players").LocalPlayer
 
-local data=game:HttpGet(whitelist)
+local data=game:HttpGet(WHITELIST_URL)
 
 if string.find(data,tostring(p.UserId),1,true) then
 
-loadstring(game:HttpGet(main))()
+loadstring(game:HttpGet(MAIN_URL))()
 
 else
 

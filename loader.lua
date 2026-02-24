907ed29ae4d12f8009ab30daac50271881bff2fa
akktel0987-boop <akktel0987@gmail.com>
2026-02-25 01:58:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,18 @@
+local WHITELIST_URL="https://raw.githubusercontent.com/akktel0987-boop/mew/main/whitelist.txt"
+local MAIN_URL="https://raw.githubusercontent.com/akktel0987-boop/mew/main/main.lua"
+
 getgenv()[string.char(95,75,69,89)]="Q6L6_PRIVATE_KEY_2025"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/akktel0987-boop/mew/main/main.lua"))()
+
+local p=game:GetService("Players").LocalPlayer
+
+local data=game:HttpGet(WHITELIST_URL)
+
+if string.find(data,tostring(p.UserId),1,true) then
+
+loadstring(game:HttpGet(MAIN_URL))()
+
+else
+
+p:Kick("Buy script in Discord: @q6l6")
+
+end
